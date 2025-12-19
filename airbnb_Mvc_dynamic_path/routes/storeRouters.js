66aaea5c9fe26f4f1fb881e95128b7b72de1423a
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-19 14:41:16 +0530

    Refactor storeRouter

--- airbnb_Mvc_dynamic_path/routes/storeRouters.js
+++ airbnb_Mvc_dynamic_path/routes/storeRouters.js
@@ -14,4 +14,13 @@ storeRouter.get("/bookings", storeController.getBookings);
 
 storeRouter.get("/favourites", storeController.getFavouriteList);
 
+storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
+
+storeRouter.post("/favourites", storeController.postAddToFavourite);
+
+storeRouter.post(
+  "/favourites/delete/:homeId",
+  storeController.postRemoveFromFavourite
+);
+
 module.exports = storeRouter;
