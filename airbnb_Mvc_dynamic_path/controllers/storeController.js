aab1fc5e43abacb1b7c9506fe02f529732aff85f
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-19 14:47:21 +0530

    refactor storeConterller

--- airbnb_Mvc_dynamic_path/controllers/storeController.js
+++ airbnb_Mvc_dynamic_path/controllers/storeController.js
@@ -1,4 +1,4 @@
-const registeredHomes = require("../routes/hostRouters");
+const Favourite = require("../models/favourite");
 const Home = require("../models/home");
 
 exports.getIndex = (req, res, next) => {
@@ -29,11 +29,51 @@ exports.getBookings = (req, res, next) => {
 };
 
 exports.getFavouriteList = (req, res, next) => {
-  Home.fetchAll((registeredHomes) => {
-    res.render("store/favourite-list", {
-      registeredHomes: registeredHomes,
-      pageTitle: "My Favourites",
-      currentPage: "favourites",
+  Favourite.getFavourites((favourites) => {
+    Home.fetchAll((registeredHomes) => {
+      const favouriteHomes = registeredHomes.filter((home) =>
+        favourites.include(home.id)
+      );
+      res.render("/store/favourite-list", {
+        favouriteHomes: favouriteHomes,
+        pageTitle: "My Favourtites",
+        currentPage: "favourites",
+      });
     });
+  });
+};
+
+exports.postAddToFavourite = (req, res, next) => {
+  Favourite.addToFavourite(req.body.id, (error) => {
+    if (error) {
+      console.log("Error while marking favourite :", error);
+    }
+    res.redirect("/favourites");
+  });
+};
+
+exports.postRemoveFromFavourite = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Favourite.deleteById(homeId, (error) => {
+    if (error) {
+      console.log("Error while removing from Favourite", error);
+    }
+    res.redirect("/favourites");
+  });
+};
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.param.homeId;
+  Home.findById(homeId, (home) => {
+    if (!home) {
+      console.log("Home not Found");
+      res.redirect("/homes");
+    } else {
+      res.render("/store/home-details", {
+        home: home,
+        pageTitle: "Home Details",
+        currentPage: "Home",
+      });
+    }
   });
 };
